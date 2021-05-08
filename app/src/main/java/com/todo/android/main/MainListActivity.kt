849a62534164ba22f2938077e061daed4ec91aad
Dongin Kim <kdi4348@naver.com>
2021-05-08 20:29:29 +0900

    2nd impl MainlistActivity
    1. To add view binding
    2. To attach toolbar and inflate menu

--- app/src/main/java/com/todo/android/main/MainListActivity.kt
+++ app/src/main/java/com/todo/android/main/MainListActivity.kt
@@ -2,11 +2,43 @@ package com.todo.android.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.todo.android.R
+import com.todo.android.databinding.ActivityMainListBinding
 
 class MainListActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_list)
+        binding = ActivityMainListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // set toolbar to appbar in this activity.
+        setSupportActionBar(binding.mainListToolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        binding.mainListToolbar.inflateMenu(R.menu.actionbar_main_list)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.main_list_toolbar_add -> {
+                // add icon
+                true
+            }
+            R.id.main_list_toolbar_search -> {
+                // search icon
+                true
+            }
+            R.id.main_list_toolbar_sort -> {
+                // sort icon
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
