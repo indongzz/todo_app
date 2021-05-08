9fd1cd847705fedcee68fd013a549906f2616ccc
Dongin Kim <kdi4348@naver.com>
2021-05-08 17:16:51 +0900

    To impl 1st MainListActivity using actionbar -> toolbar

--- app/src/main/java/com/todo/android/main/MainListActivity.kt
+++ app/src/main/java/com/todo/android/main/MainListActivity.kt
@@ -1,11 +1,12 @@
-package com.todo.android
+package com.todo.android.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.todo.android.R
 
-class MainActivity : AppCompatActivity() {
+class MainListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_list)
     }
 }
